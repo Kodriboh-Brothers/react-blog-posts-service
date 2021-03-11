a08bea56dbc015f4048bdda7041f6e2f2e9e5c90
LukeMcCann <developer-lukemccann@outlook.com>
2021-03-11 20:12:26 +0000

    Add basic event received endpoint

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = require('express')();
 app.use(bodyParser.json());
@@ -19,7 +20,7 @@ app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     // Generate random id
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
@@ -28,8 +29,21 @@ app.post('/posts', (req, res) => {
         id, title
     };
 
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    });
+
     // Return created and post created
     res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+    console.log('ReceivedEvent', req.body.type);
+
+    res.send({});
 });
 
 app.listen(port, () => {
